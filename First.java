c332ea89447d904605550e6b7b66c3c2461c50c8
Asif Hussain <158553805+asifmeer55766@users.noreply.github.com>
2026-01-12 19:05:02 +0500

    initial commit

---
+++ First.java
@@ -0,0 +1,10 @@
+
+public class First {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Guys!");
+        System.out.println("This is first lecture of Data Structure and Algorithems in Java.");
+
+    }
+}
